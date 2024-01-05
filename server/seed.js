e12af9faaee065afad460465d45c2e7ec38a09b7
Ramon Bezerra <ramonbezerra90@gmail.com>
2024-01-05 11:53:23 -0300

    feat(database): criando conexão e seed inicial

--- server/seed.js
+++ server/seed.js
@@ -1,8 +1,38 @@
 const fs = require("fs");
+const dotenv = require("dotenv");
+const MongoClientAdapter = require("./src/infra/database/MongoClientAdapter");
 
+dotenv.config();
 const movies = fs.readFileSync("./movies-seed.json");
 const jsonMovies = JSON.parse(movies);
 
-module.exports = () => {
-  
-}
+const dbClient = MongoClientAdapter().getInstance();
+
+const insertMovie = async (movie, collection) => {
+  const result = await collection.insertOne(movie);
+  console.log(`Filme ${movie.name} inserido com o id: ${result.insertedId}`);
+};
+
+const main = async () => {
+  try {
+    const database = await dbClient.db("locadora");
+    const movies = await database.collection("movies");
+
+    if ((await movies.count()) > 0) {
+      console.log("Banco já populado");
+      process.exit();
+    }
+
+    for (const movie of jsonMovies) {
+      await insertMovie(movie, movies);
+    }
+  } catch (err) {
+    console.log("ERRO", err);
+  } finally {
+    await dbClient.close();
+    console.log("conexão encerrada");
+    process.exit();
+  }
+};
+
+main();
---
+++ server/src/infra/database/MongoClientAdapter.js
@@ -0,0 +1,22 @@
+const { MongoClient } = require("mongodb");
+
+const MongoClientAdapter = (function () {
+  let instance;
+
+  function createInstance() {
+    const uri = `mongodb://localhost:${process.env.MONGODB_PORT}`;
+    return new MongoClient(uri, { connectTimeoutMS: 30000 }, { keepAlive: 1 });
+  }
+
+  return {
+    getInstance: function () {
+      if (!instance) {
+        instance = createInstance();
+      }
+
+      return instance;
+    },
+  };
+})();
+
+module.exports = () => MongoClientAdapter;
